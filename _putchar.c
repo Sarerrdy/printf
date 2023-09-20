89e5253943f7aa7624764f5c1990edc9206e43df
Edmond <sarerrdy4live@gmail.com>
2023-09-19 20:01:18 -0700

    update betty

--- _putchar.c
+++ _putchar.c
@@ -3,10 +3,11 @@
 /**
  * _putchar -  writes the character c to stdout
  * @c: the character to print
+ * Return: c count
  */
 
 int _putchar(char c)
 {
 	write(1, &c, 1);
-	return 1;
+	return (1);
 }
